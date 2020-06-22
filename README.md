fd23ea38c85f6130934f1a06610dafe5624ad0e4
Josh Gretz <jgretz@truefit.io>
2020-06-22 12:50:58 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# @jgretz/github-rename
+
+## Overview
+
+this a node utility to rename all of main branches of my repos to main from master
---
+++ src/types.ts
@@ -0,0 +1,24 @@
+import {FunctionComponent, ReactNode} from 'react';
+
+export type StringKeyMap<T> = {[key: string]: T};
+
+export type DependencyList<T> = Array<keyof T> | undefined;
+export type ComponentTransform = (component: FunctionComponent | undefined) => FunctionComponent;
+export type RenderTransform = (previousStatement: string | undefined) => string;
+
+export type EnhancerResult = {
+  dependencies: StringKeyMap<unknown>;
+  initialize: string;
+  props: string[];
+  transformRender?: RenderTransform;
+  transformComponent?: ComponentTransform;
+};
+
+export type EnhancerContext = {
+  component: ReactNode;
+  generateNewVariable: () => string;
+};
+
+export type Enhancer = (context: EnhancerContext) => EnhancerResult;
+
+export type EnhancerList = Enhancer[] | (Enhancer | Enhancer[])[];
